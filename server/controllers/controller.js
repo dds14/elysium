ede30c68fa14c912587b2f754a5bd53a5df70615
Derek Sams <dereksams@Dereks-MacBook-Pro.local>
2019-07-26 18:41:00 -0500

    server successfully started

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -5,7 +5,7 @@ const addToNotes = (req, res) => {
   req.body.id = notes.length + 1;
   notes.push(req.body);
   res.status(200).json(notes);
-  console.log("DEREasdfadsfsadfsdK HELLOOSSS");
+  console.log("Note successfully added");
 };
 
 const receivePublicToken = (req, res) => {
