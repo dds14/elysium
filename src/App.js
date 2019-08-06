a952d733979040fca616aa88336da199ad442017
Derek Sams <dereksams@Dereks-MacBook-Pro.local>
2019-08-06 14:23:57 -0400

    cleaning up

--- src/App.js
+++ src/App.js
@@ -5,16 +5,12 @@ import "./App.css";
 
 // Component imports (okay to delete if not used)
 import Navbar from "./components/Navbar/Navbar";
-import Form from "./components/Form/Form";
-import Link from "./components/Link/Link";
 
 function App() {
   return (
     <HashRouter>
       <div className="App">
         <Navbar />
-        {/* <Form /> */}
-        {/* <Link /> */}
         {routes}
       </div>
     </HashRouter>
